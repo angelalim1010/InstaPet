182c6e1baa5543bee0d63eeacc7727a28012852c
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2019-06-14 15:23:59 -0400

    user actions made

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,2 +1,67 @@
 import axios from "axios";
-import { GET_USERS, ADD_USER, REMOVE_USER, EDIT_USER } from "./types";
+import {
+  GET_USERS,
+  GET_USER_POSTS,
+  ADD_USER_POST,
+  REMOVE_USER_POST,
+  ADD_USER,
+  REMOVE_USER,
+  EDIT_USER
+} from "./types";
+
+// Users
+
+const getUsers = users => {
+  return {
+    type: GET_USERS,
+    payload: users
+  };
+};
+
+const addUser = newUser => {
+  return {
+    type: ADD_USER,
+    payload: newUser
+  };
+};
+
+const removeUser = userId => {
+  return {
+    type: REMOVE_USER,
+    payload: userId
+  };
+};
+
+const editUser = editedUser => {
+  return {
+    type: EDIT_USER,
+    payload: editedUser
+  };
+};
+
+// User Posts
+
+const getUserPosts = posts => {
+  return {
+    type: GET_USER_POSTS,
+    payload: posts
+  };
+};
+
+const addUserPost = newPost => {
+  return {
+    type: ADD_USER_POST,
+    action: newPost
+  };
+};
+
+const removeUserPost = postId => {
+  return {
+    type: REMOVE_USER_POST,
+    action: postId
+  };
+};
+
+// USER THUNKS
+
+// USER POST THUNKS
