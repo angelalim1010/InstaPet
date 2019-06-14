689b1b1ba792d17038ae4369baddc6a66453d1b0
Tony Fung <tfung5@gmail.com>
2019-06-14 11:35:46 -0400

    Installed a couple redux packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
-import NavigationBar from "./components/layout/NavigationBar";
+import NavBar from "./components/layout/NavBar";
 import Homepage from "./components/layout/Homepage";
 
 export default class App extends Component {
@@ -8,9 +8,8 @@ export default class App extends Component {
     return (
       <Router>
         <div>
-          <NavigationBar />
-          <Route exact path="/" component={Homepage}>
-          </Route>
+          <NavBar />
+          <Route exact path="/" component={Homepage} />
         </div>
       </Router>
     );
