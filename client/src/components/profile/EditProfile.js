003bcd7961fa62ee9d9741e5d0b1e0b6073bfb67
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2019-06-14 11:46:17 -0400

    creating login branch

---
+++ client/src/components/profile/EditProfile.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class EditProfile extends Component{
+    render(){
+        return(
+            <h1>edit page</h1>
+        )
+    }
+}
+
+export default EditProfile;
