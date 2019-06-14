c015706dff30b862703bfd356633ccc4ec7423bc
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2019-06-14 12:15:27 -0400

    initial user done html part only

--- client/src/components/layout/HomePage.js
+++ client/src/components/layout/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import UserProfile from "../profile/UserProfile";
 
 export default class HomePage extends Component {
   render() {
---
+++ client/src/components/profile/UserProfile.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+export default class UserProfile extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "Bob Anderson",
+      email: "blah@mcblah.com",
+      imageUrl: "../../img/fetchDog.png",
+      bio: "I am cool , follow me :)",
+      followers: 5,
+      following: 5
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="row top">
+          <div className="col-sm-6">
+            <h1 className="name">{this.state.name}</h1>
+            <img src={this.state.imageUrl} alt="profile" />
+            <a href="#">Edit Profile</a>
+          </div>
+          <div className="col-sm-6">
+            <h2>Bio:</h2>
+            <p>{this.state.bio}</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-6">
+            <h3>Followers: {this.state.followers}</h3>
+          </div>
+          <div className="col-sm-6">
+            <h3>Following: {this.state.following}</h3>
+          </div>
+        </div>
+        <div className="row">
+          <p>Posts go here</p>
+        </div>
+      </div>
+    );
+  }
+}
