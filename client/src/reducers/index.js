56e87fd612ea876af90e678e5f219e57dfc33e1c
unknown <tfung5@gmail.com>
2019-06-15 23:00:32 -0400

    Removed homePageReducer from index reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
-import homePageReducer from "./homePageReducer";
 import userReducer from "./userReducer";
 import postReducer from "./postReducer";
 
 const rootReducer = combineReducers({
-  homePage: homePageReducer,
   user: userReducer,
   post: postReducer
 });
