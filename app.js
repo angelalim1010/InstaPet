14a9975a372077a1462ef792817daccfd273aad2
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2019-06-16 19:21:29 -0400

    app after pull from master

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const logger = require("morgan");
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
 
 // .env config
-require("dotenv").config();
+require('dotenv').config();
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 // Parse requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Configure routes
-require("./database/routes")(app);
+require('./database/routes')(app);
 
 module.exports = app;
