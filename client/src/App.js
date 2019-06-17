4bc9e5cb774c4e6197bcb324b34e275fc97f19c3
Mary Fan <maryfan@infires2.local>
2019-06-17 15:31:26 -0400

    fixed merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,7 @@ import NavBar from "./components/navbar/NavBar";
 import HomePage from "./components/homepage/HomePage";
 import Login from "./components/login/Login";
 import SignUp from "./components/login/SignUp";
-<<<<<<< HEAD
-import Post from "./components/post/Post";
-=======
 import Profile from "./components/profile/Profile";
->>>>>>> 18733a12b6a9fbf184622280b8b95f771e925b19
 
 export default class App extends Component {
   render() {
@@ -21,11 +17,7 @@ export default class App extends Component {
             <Route exact path="/" component={HomePage} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={SignUp} />
-<<<<<<< HEAD
-            <Route exact path="/p/:postId" component={Post} />
-=======
             <Route exact path="/profile" component={Profile} />
->>>>>>> 18733a12b6a9fbf184622280b8b95f771e925b19
           </div>
         </div>
       </Router>
