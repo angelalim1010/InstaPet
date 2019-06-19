f6f7aad4a56040846138c25117feda8546f55022
unknown <tfung5@gmail.com>
2019-06-19 00:21:24 -0400

    Added FindAllPosts endpoint

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
+// Set Post model
+const { Post } = require("../database/models");
+
+/**
+ * FindAllPosts endpoint
+ * @route GET /p
+ * @desc Find all posts
+ * @access Public
+ */
+router.get("/", (req, res, next) => {
+  return Post.findAll({
+    order: [["id", "DESC"]]
+  })
+    .then(posts => res.status(200).json(posts))
+    .catch(err => res.status(400).json(err));
+}); // End FindAllPosts endpoint
+
 /**
  * POST ROUTES
  */
