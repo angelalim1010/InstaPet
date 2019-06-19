f0540cb45f86c99dc34a9c214d7cf665bd936dbe
Tony Fung <tfung5@gmail.com>
2019-06-19 17:31:12 -0400

    Modified navbar to include link to the profile of the current user

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -36,28 +36,27 @@ class NavBar extends Component {
           </Form>
 
           <div className="navBarProfile navBarItem">
-            <Link to="/profile">
-              <FontAwesomeIcon icon={faUserEmpty} size="2x" color="black"/>
+            <Link to={"/profile/" + this.props.auth.user.userName}>
+              <FontAwesomeIcon icon={faUserEmpty} size="2x" color="black" />
             </Link>
           </div>
 
           <Button className="navBarItem" onClick={this.logout}>
             Logout
           </Button>
-
         </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = state => ({
+  auth: state.auth
+});
 
-const mapDispatchToProps = dispatch => {
-  return {
-    logoutUser: () => dispatch(logoutUser())
-  };
-};
+const mapDispatchToProps = dispatch => ({
+  logoutUser: () => dispatch(logoutUser())
+});
 
 export default connect(
   mapStateToProps,
