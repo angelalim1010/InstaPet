3b155cd0d0f66d7f4794ed851e9c26009e8f1cad
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2019-06-19 23:45:42 -0400

    reverting back merge changes

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import "./NavBar.css";
-import { Form, Input, Button } from "reactstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser as faUserEmpty } from "@fortawesome/free-regular-svg-icons";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
+import { Form, Input, Button } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser as faUserEmpty } from '@fortawesome/free-regular-svg-icons';
 import { faDoorOpen } from "@fortawesome/free-solid-svg-icons";
-import { logoutUser } from "../../actions/authActions";
+import { logoutUser } from '../../actions/authActions';
 
 class NavBar extends Component {
   constructor(props) {
@@ -24,11 +24,8 @@ class NavBar extends Component {
         <div className="navBarItems">
           <div className="navBarLogo navBarItem">
             <Link to="/">
-              <img
-                className="logo"
-                src={require('../../img/logo.png')}
-                alt="logo"
-              />
+              <img className = "logo"
+                src={require("../../img/logo.png")}/>
             </Link>
           </div>
 
@@ -37,14 +34,14 @@ class NavBar extends Component {
           </Form>
 
           <div className="navBarProfile navBarItem">
-            <Link to="/profile">
-              <FontAwesomeIcon className="navBarIcon" icon={faUserEmpty}/>
+            <Link to={'/profile/' + this.props.auth.user.userName}>
+              <FontAwesomeIcon icon={faUserEmpty}/>
             </Link>
-
-              <Button className="navBarItem" onClick={this.logout}>
-                 <FontAwesomeIcon className="navBarIcon" icon={faDoorOpen}/>
-              </Button>
           </div>
+
+          <Button className="navBarItem" onClick={this.logout}>
+            <FontAwesomeIcon className="navBarIcon" icon={faDoorOpen}/>
+          </Button>
         </div>
       </div>
     );
